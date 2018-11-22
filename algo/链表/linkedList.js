6545af68795817abf3ca862611d0274260e392d6
xiaolei <1017653702@qq.com>
2018-11-22 10:51:13 +0800

    update

---
+++ algo/链表/linkedList.js
@@ -0,0 +1,49 @@
+class Node {
+  constructor(element) {
+    this.element = element
+    this.next = null
+  }
+}
+
+class LinkedList {
+  constructor(element) {
+    this.head = new Node(element)
+  }
+
+  find(element) {
+    let curNode = this.head
+    while (curNode && (curNode.element !== element)) {
+      curNode = curNode.next
+    }
+    return (curNode && (curNode.element === element)) ? curNode : null
+  }
+
+  insert(newElement, node) {
+    const newNode = new Node(newElement)
+    if (!node) {
+      let lastNode = this.head
+      while(lastNode.next)
+        lastNode = lastNode.next
+      lastNode.next = newNode
+    } else {
+      const _node = this.find(node)
+      const _nextNode = _node.next
+      newNode.next = _nextNode
+      _node.next = newNode
+    }
+  }
+
+  display() {
+    let curNode = this.head
+    while(curNode) {
+      console.log(curNode.element)
+      curNode = curNode.next
+    }
+  }
+}
+
+const linkedList = new LinkedList('new LinkedList')
+linkedList.insert('影魔')
+linkedList.insert('幽鬼', '影魔')
+linkedList.insert('SA', '影魔')
+linkedList.display()
