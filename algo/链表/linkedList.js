41e24b065e94a219462bbdcb668ecdafbecf5dfa
xiaolei <1017653702@qq.com>
2018-11-23 10:37:51 +0800

    update

--- algo/链表/linkedList.js
+++ algo/链表/linkedList.js
@@ -40,10 +40,26 @@ class LinkedList {
       curNode = curNode.next
     }
   }
+
+  findPrevious(element) {
+    let curNode = this.head
+    while (curNode && curNode.next && curNode.next.element !== element) {
+      curNode = curNode.next
+    }
+    return (curNode && curNode.next && (curNode.next.element === element)) ? curNode : null
+  }
+
+  remove(element) {
+    let node = this.findPrevious(element)
+    node && (node.next = node.next.next)
+  }
 }
 
 const linkedList = new LinkedList('new LinkedList')
 linkedList.insert('影魔')
 linkedList.insert('幽鬼', '影魔')
 linkedList.insert('SA', '影魔')
+linkedList.remove('SA')
 linkedList.display()
+// const node = linkedList.findPrevious('dd')
+// console.log(node)
