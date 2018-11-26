2afa08a00b5904e7b4badd992a538f0d9881673d
xiaolei <1017653702@qq.com>
2018-11-26 14:57:34 +0800

    update

---
+++ algo/链表/curcir-linked-list.js
@@ -0,0 +1,100 @@
+class Node {
+  constructor(element) {
+    this.element = element
+    this.next = null
+  }
+}
+
+class LinkedList {
+  constructor(element) {
+    this.head = new Node(element)
+    this.current = this.head
+    this.length = 1
+  }
+
+  find(element) {
+    let curNode = this.head
+    while (curNode && (curNode.element !== element)) {
+      curNode = curNode.next
+    }
+    return (curNode && (curNode.element === element)) ? curNode : null
+  }
+
+  insert(newElement, node) {
+    const newNode = new Node(newElement)
+    if (!node) {
+      let lastNode = this.head
+      while(lastNode.next && lastNode.next !== this.head) {
+        lastNode = lastNode.next
+      }
+      lastNode.next = newNode
+      newNode.next = this.head
+      // newNode.previous = lastNode
+    } else {
+      const _node = this.find(node)
+      const _nextNode = _node.next
+      newNode.next = _nextNode
+      // _nextNode && (_nextNode.previous = newNode)
+      _node.next = newNode
+      // newNode.previous = _node
+    }
+    ++this.length 
+  }
+
+  display() {
+    let curNode = this.head
+    while(curNode) {
+      console.log(curNode.element)
+      curNode = curNode.next
+      if (curNode === this.head)
+        break
+    }
+  }
+
+  findPrevious(element) {
+    let curNode = this.head
+    while (curNode && curNode.next && curNode.next.element !== element) {
+      curNode = curNode.next
+      if (curNode === this.head)
+        break
+    }
+    return (curNode && curNode.next && (curNode.next.element === element)) ? curNode : null
+  }
+
+  remove(element) {
+    let node = this.findPrevious(element)
+    node && (node.next = node.next.next)
+    console.log(this)
+    --this.length
+  }
+
+  next() {
+    this.current = this.current.next
+    return this.current
+  }
+}
+
+const linkedList = new LinkedList(1)
+for(let i = 2; i <= 10; i++) {
+  linkedList.insert(i)
+}
+
+function killNumThird() {
+  if (linkedList.length < 3) {
+    return
+  }
+  let index = 2
+  while(index--) {
+    let node = linkedList.next()
+    if (!index) {
+      if (node.element === linkedList.head.element) {
+        linkedList.head = node.next
+      }
+      linkedList.remove(node) 
+      killNumThird()
+    }
+  }
+}
+
+killNumThird()
+console.log(linkedList)
