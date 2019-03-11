aaa0986946a426923347e08b2fde8a11ae8c4a35
xiaolei <1017653702@qq.com>
2019-03-11 10:03:04 +0800

    update

--- algo/tree/bst.js
+++ algo/tree/bst.js
@@ -104,6 +104,48 @@ for (let i = 0; i < arr.length; i++) {
   bst.insert(arr[i])
 }
 
-console.log(bst.find(11))
+// 1.1 计算一个二叉树的层数
+// 递归版本抽象：取某个节点，取这个节点的左子树和右子树的最大高度，再加1，即为这个节点的高度
 
-// TODO: 计算一个二叉树的层数
+function calBSTHeight1(node) {
+  if (node) {
+    return Math.max(calBSTHeight1(node.left), calBSTHeight1(node.right)) + 1
+  }
+  return 0
+}
+
+console.log(calBSTHeight1(bst.root))
+
+// 1.2 计算一个二叉树的层数
+// 非递归版本抽象：从 root 节点开始遍历，每遍历一层，层数+1
+// 同时计算每一层节点的数量，直到遇到某一层节点数量为0的时候，即可判断已经为BST最底层的节点了
+
+function calBSTHeight2(node) {
+  let nodeArr = []
+  nodeArr.push(node.root)
+  let height = 1
+  while(nodeArr.length > 0) {
+    let len = nodeArr.length
+    let index = 0
+
+    ++height
+
+    while (index < len) {
+      const _node = nodeArr[index]
+      if (_node.left) {
+        nodeArr.push(_node.left)
+      }
+      if (_node.right) {
+        nodeArr.push(_node.right)
+      }
+      ++index
+      nodeArr.shift()
+    }
+  }
+
+  return height
+}
+
+console.log(calBSTHeight2(bst))
+
+// 2. 计算某个给定的BST节点的所处的高度
